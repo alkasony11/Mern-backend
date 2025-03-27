287966dab1893528e6dbc6694047ad0acc2e3920
Alka <alkasony2026@mca.ajce.in>
2025-03-27 11:52:44 +0530

    Initial commit for MERN project

---
+++ Models/Item.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+
+
+const itemSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    location: {
+        type: String,
+        required: true,
+    },
+    price: {
+        type: Number,
+        required: true,
+    }   
+});
+
+const Item = mongoose.model("Item", itemSchema);
+
+module.exports = Item;
+    
